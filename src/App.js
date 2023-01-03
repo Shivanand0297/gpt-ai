298c9441d04324559706bab933b5566cb82ce90d
Shivanand <Shivanand0297@gmail.com>
2023-01-04 00:16:35 +0530

    called all the components

--- src/App.js
+++ src/App.js
@@ -7,7 +7,19 @@ import { CTA, Brand, Navbar } from './components'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App' >
+      <div className='gradient_bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibilities/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
+    </div>
   )
 }
 
