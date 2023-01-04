c5ba2b8dfe0efa61652ded392c4a48582f330c4a
Shivanand <Shivanand0297@gmail.com>
2023-01-04 22:39:31 +0530

    worked on brands section

---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import atlassian from "../../assets/atlassian.png"
+import dropbox from "../../assets/dropbox.png"
+import google from "../../assets/google.png"
+import shopify from "../../assets/shopify.png"
+import slack from "../../assets/slack.png"
+
+export {
+    atlassian,
+    dropbox,
+    google,
+    shopify,
+    slack
+}
