3a01855ebd990da87e59a58f4e165175c07e0d7c
Shivanand <Shivanand0297@gmail.com>
2023-01-05 22:10:53 +0530

    changed id

--- src/containers/possibilities/Possibilities.jsx
+++ src/containers/possibilities/Possibilities.jsx
@@ -4,7 +4,7 @@ import possibilityImage from '../../assets/possibility.png'
 
 const Possibilities = () => {
   return (
-    <div className="gpt3__possibility section__padding" id="possibility">
+    <div className="gpt3__possibility section__padding" id="possiblities">
       <div className="gpt3__possibility-image">
         <img src={possibilityImage} alt="possibility" />
       </div>
